38057f158c4cf17629b031b19cffd9255845bfa3
Vlad-Siv <siv.default@gmail.com>
2020-12-26 15:57:31 +0200

    wip auth

--- src/redux/reduces/auth/index.js
+++ src/redux/reduces/auth/index.js
@@ -1,13 +1,22 @@
 import { createReducer } from '../../../helpers/methods';
+import { setUserToken } from '../../../services/api';
 
 const initialState = {
   isLoadingLogin: false,
 };
 
+const loginSuccess = (state, { result }) => {
+  setUserToken(result.data.data.token);
+  return {
+    ...state,
+    isLoadingLogin: false,
+  }
+}
+
 const authReducer = createReducer(initialState,
   {
     LOGIN_START: (state) => ({ ...state, isLoadingLogin: true }),
-    LOGIN_SUCCESS: (state) => ({ ...state, isLoadingLogin: false }),
+    LOGIN_SUCCESS: loginSuccess,
     LOGIN_FAIL: (state) => ({ ...state, isLoadingLogin: false }),
   });
 
